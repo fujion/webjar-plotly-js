d9046dfec3f08c331d5284ceb101c0a26b353f4d
Douglas Martin <mdgeek1@gmail.com>
2023-12-29 00:14:25 -0500

    Retrieve minified version from NPM.

--- staging/gulpfile.js
+++ staging/gulpfile.js
@@ -1,46 +1,33 @@
 const {src, dest, series} = require('gulp');
-const minifyJS = require('gulp-minify');
 
-const srcDir = './node_modules/plotly.js-dist/';
+const srcDir1 = './node_modules/plotly.js-dist/';
+const srcDir2 = './node_modules/plotly.js-dist-min/';
 const destDir = '${webjar.target}/';
 
 function task1() {
-    return _copy(['*.md', 'LICENSE'])
+    return _copy(srcDir1, ['*.md', 'LICENSE'])
 }
 
 function task2() {
-    return _minifyJS('*.js', 'dist');
+    return _copy(srcDir1, '*.js', 'dist');
 }
 
 function task3() {
-    return _minifyCSS('*.css', 'dist');
+    return _copy(srcDir2, '*.js', 'dist');
 }
 
-function _toSrc(_src) {
-    return src(_src, {allowEmpty: false, cwd: srcDir});
+function _toSrc(_srcDir, _src) {
+    return src(_src, {allowEmpty: false, cwd: _srcDir});
 }
 
 function _toDest(_dest) {
     return dest(_dest || '.', {cwd: destDir});
 }
 
-function _copy(_src, _dest) {
+function _copy(_srcDir, _src, _dest) {
     console.log('  Copying ' + _src);
-    return _toSrc(_src).pipe(_toDest(_dest))
+    return _toSrc(_srcDir, _src).pipe(_toDest(_dest))
 }
 
-function _minifyJS(_src, _dest) {
-    console.log('  Minifying ' + _src);
-    return _toSrc(_src)
-        .pipe(_toDest(_dest))
-        .pipe(minifyJS(
-            {
-                noSource: true,
-                ext: {
-                    min: '.min.js'
-                }
-            }))
-        .pipe(_toDest(_dest))
-}
 
-exports.default = series(task1, task2);
+exports.default = series(task1, task2, task3);
